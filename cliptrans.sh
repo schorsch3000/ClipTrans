964723f33ef70cf3526c0b0cd3fecd98166de8d1
Dirk Heilig <heilig@adesso-mobile.de>
2019-10-18 12:57:21 +0200

    Fail gracefully

--- cliptrans.sh
+++ cliptrans.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd "$(dirname "$(realpath "$0")")"
+cd "$(dirname "$(realpath "$0")")" || exit 1
 transformer="$(find transformers -type f -name "transform" -executable | cut -d/ -f2 | sort | rofi -dmenu "$@")"
 test -x "transformers/$transformer/transform" || exit 1
 xclip -o  -selection clipboard | "transformers/$transformer/transform" 2>/tmp/cliptransErr | xclip -i -selection clipboard
