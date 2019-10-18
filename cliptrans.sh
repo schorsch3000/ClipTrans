fea6ce2551cdb375d5f97ea18508245920f85a3f
Dirk Heilig <heilig@adesso-mobile.de>
2019-10-18 12:59:33 +0200

    Send notification when done

--- cliptrans.sh
+++ cliptrans.sh
@@ -5,6 +5,7 @@ transformer="$(find transformers -type f -name "transform" -executable | cut -d/
 test -x "transformers/$transformer/transform" || exit 1
 xclip -o  -selection clipboard | "transformers/$transformer/transform" 2>/tmp/cliptransErr | xclip -i -selection clipboard
 if [ -s /tmp/cliptransErr ] ; then
-  notify-send "cliptrans" "Transformer $transformer failed, see /tmp/cliptransErr"
+  notify-send -u critical "cliptrans" "Transformer $transformer failed, see /tmp/cliptransErr"
 fi
+notify-send -u low -t 2500 "cliptrans" "Done: $(xclip -o -selection clipboard)"
 
