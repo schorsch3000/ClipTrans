83cd6c7ddd658efd68cc5f64b0ebbe98967923fe
Dirk Heilig <heilig@adesso-mobile.de>
2019-10-18 13:11:52 +0200

    Add the current clip-content to rofi

--- cliptrans.sh
+++ cliptrans.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "$(dirname "$(realpath "$0")")" || exit 1
-transformer="$(find transformers -type f -name "transform" -executable | cut -d/ -f2 | sort | rofi -dmenu "$@")"
+transformer="$(find transformers -type f -name "transform" -executable | cut -d/ -f2 | sort | rofi -dmenu -mesg "Transforming: $(xclip -o -selection clipboard)" "$@")"
 test -x "transformers/$transformer/transform" || exit 1
 xclip -o  -selection clipboard | "transformers/$transformer/transform" 2>/tmp/cliptransErr | xclip -i -selection clipboard
 if [ -s /tmp/cliptransErr ] ; then
