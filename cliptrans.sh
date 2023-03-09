88ce5b3cbecc115070ce29644b0dd115a6c1f84c
Dirk Heilig <heilig@adesso-mobile.de>
2023-03-09 09:51:15 +0100

    add cli option

--- cliptrans.sh
+++ cliptrans.sh
@@ -1,15 +1,47 @@
 #!/bin/bash
 cd "$(dirname "$(realpath "$0")")" || exit 1
+set -x
+
+selector() {
+
+	if [[ "$isInteractive" == "true" ]]; then
+		selector_fzf "$@"
+	else
+		selector_rofi "$@"
+	fi
+}
+
+selector_fzf() {
+	fzf --prompt "Transforming: '$(xclip -o -selection clipboard)'" "$@" |
+		cut -d' ' -f 1
+}
+
+selector_rofi() {
+	rofi -dmenu -mesg "Transforming: '$(xclip -o -selection clipboard)'" "$@" |
+		cut -d' ' -f 1
+}
+
+export isInteractive
+
+if [ -t 0 ]; then
+	isInteractive="true"
+else
+	isInteractive="false"
+
+fi
+
 transformer="$(find transformers -mindepth 2 -maxdepth 2 -type f -name "transform" -executable |
 	cut -d/ -f2 |
 	sort |
 	{
 		while IFS= read -r transformer; do
-			echo "$transformer => '$(xclip -o -selection clipboard | timeout 2s "transformers/$transformer/transform" | head -n1)'"
+			echo "$transformer => '$(xclip -o -selection clipboard | timeout 2 "transformers/$transformer/transform" | head -n1)'" | sponge &
 		done
-	} |
-	rofi -dmenu -mesg "Transforming: '$(xclip -o -selection clipboard)'" "$@" |
-	cut -d' ' -f 1)"
+		wait
+	} | selector "$@")"
+
+echo "$transformer"
+exit
 test -x "transformers/$transformer/transform" || exit 1
 xclip -o -selection clipboard |
 	"transformers/$transformer/transform" 2>/tmp/cliptransErr |
